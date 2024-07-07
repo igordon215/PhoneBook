05a1f3a59a7c99ae90708fe9460cca0122016b8c
igordon215 <igordon215@gmail.com>
2024-07-07 18:30:53 -0400

    lab done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -16,39 +16,60 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        List<String> numbers = new ArrayList<>();
+        numbers.add(phoneNumber);
+        phonebook.put(name, numbers);
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        List<String> numbers = new ArrayList<>();
+        for (String number : phoneNumbers){
+            numbers.add(number);
+        }
+        phonebook.put(name, numbers);
     }
 
     public void remove(String name) {
+        phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        Boolean present = false;
+        if (phonebook.containsKey(name)){
+            present = true;
+        }
+        return present;
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+        for (String key : phonebook.keySet()){
+            if (phonebook.get(key).contains(phoneNumber)){
+                return key;
+            }
+        }
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> output = new ArrayList<>();
+        output.addAll(phonebook.keySet());
+        return output;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
